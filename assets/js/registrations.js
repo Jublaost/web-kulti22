5070b11b5328d13804ff7d612c23b078d9371b1d
Thomas <65050898+tvonment@users.noreply.github.com>
2022-08-26 13:33:02 +0000

    bugfix / schminke registration

--- assets/js/registrations.js
+++ assets/js/registrations.js
@@ -4,15 +4,15 @@
     console.log(data)
     for (let player of data.spikeball) {
         let container = document.getElementById(player.field);
-        let element = document.createElement("p")
-        element.innerHTML = `${player.name}`
-        container.append(element)
+        let element = document.createElement("p");
+        element.innerHTML = `${player.name}`;
+        container.appendChild(element);
     }
 
     let spikes = document.getElementById("spikeball");
     for (let spike of data.gl) {
         let element = document.createElement("p")
-        element.innerHTML = `${spike.name} - ${spike.player1}, ${spike.player2}`
+        element.innerHTML = `<strong>${spike.name}</strong> - ${spike.player1}, ${spike.player2}`
         spikes.append(element)
     }
 })();
